18180f3753c9dd2a3defb8fe4730b31ec3aaf562
Flavio <f.esposito101@studenti.unisa.it>
2019-10-31 13:07:25 +0100

    Mockup_Ionic: carrello attivo, primo prototipo

--- codebase/scanApp/src/app/pages/cart/cart.page.ts
+++ codebase/scanApp/src/app/pages/cart/cart.page.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  public isEmptyCart: boolean;
+
+  constructor() {
+    this.isEmptyCart = true;
+  }
 
   ngOnInit() {
   }
