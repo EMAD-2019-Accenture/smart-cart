7027f442b4449f31a4551d4d66e25d83814ce6a4
Flavio <f.esposito101@studenti.unisa.it>
2019-11-02 20:19:27 +0100

    Mockup_ionic: aggiunta vista su prodotti raccomandati

---
+++ codebase/scanApp/src/app/pages/recommendation/recommendation.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation',
+  templateUrl: './recommendation.page.html',
+  styleUrls: ['./recommendation.page.scss'],
+})
+export class RecommendationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
