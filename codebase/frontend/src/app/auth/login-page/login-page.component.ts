ef9c725cb3934023edb946a537f9d26fa9c939ed
Flavio <f.esposito101@studenti.unisa.it>
2019-12-07 18:38:31 +0100

    Front-end: redirezione al carrello post login e piccoli aggiustamenti UI

--- codebase/frontend/src/app/auth/login-page/login-page.component.ts
+++ codebase/frontend/src/app/auth/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user';
 import { AuthService } from '../auth.service';
 import {ToastNotificationService} from '../../shared/toast/toast-notification.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,7 +13,7 @@ export class LoginPageComponent implements OnInit {
 
   model: User = { password: '', username: '' };
 
-  constructor(private authService: AuthService, public toastNotificationService: ToastNotificationService) { }
+  constructor(private authService: AuthService, private toastNotificationService: ToastNotificationService, private router: Router) { }
 
   ngOnInit() {
     /* Check Login through AuthService */
@@ -20,12 +21,15 @@ export class LoginPageComponent implements OnInit {
 
   login() {
     let toast = this.toastNotificationService;
+    let router = this.router;
+
     this.authService.login(this.model)
       .subscribe({
         next(logInfo) {
           let message = "Login effettuato!";
           let color = "success";
-          toast.presentToast(message, color); 
+          toast.presentToast(message, color);
+          router.navigate(['/carrello'])
         },
         error(err) {
           let message = "Login fallito!";
