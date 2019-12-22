ba06e08ac75496f88b02ab44958a52048dd4027d
emaiannone <emaiannone96@gmail.com>
2019-12-22 17:01:48 +0100

    Add filter in catalogo page

--- codebase/frontend/src/app/catalogo/catalogo-page/catalogo-page.component.ts
+++ codebase/frontend/src/app/catalogo/catalogo-page/catalogo-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/shared/model/category';
+import { Product } from 'src/app/shared/model/product';
 import { CatalogoService } from '../catalogo.service';
 
 @Component({
@@ -9,17 +10,39 @@ import { CatalogoService } from '../catalogo.service';
 })
 export class CatalogoPageComponent implements OnInit {
 
+  allProducts: Array<Product>;
+  filteredProducts: Array<Product>;
   categories: Array<Category>;
 
   constructor(private catalogoService: CatalogoService) {
+    this.allProducts = new Array<Product>();
+    this.filteredProducts = new Array<Product>();
     this.categories = new Array<Category>();
   }
 
   ngOnInit() {
     this.catalogoService.getCategories().then((value) => {
-      this.categories = value;
-      console.log(this.categories);
+      this.categories = value.map(category => new Category(category));
+    });
+    this.catalogoService.getProducts().then(value => {
+      this.allProducts = value.map(product => new Product(product));
+      this.filteredProducts = this.allProducts;
     });
   }
 
+  public filter(searchBar: HTMLInputElement, dropdownCategory: HTMLInputElement): void {
+    this.filteredProducts = this.allProducts;
+    const selectedIndex: number = Number(dropdownCategory.value);
+    if (selectedIndex !== 0) {
+      const category: Category = this.categories[selectedIndex - 1];
+      this.filteredProducts = this.filteredProducts.filter(product =>
+        product.getCategory().getId() === category.getId()
+      );
+    }
+    const keyword: string = searchBar.value;
+    this.filteredProducts = this.filteredProducts.filter(product => {
+      const regex = new RegExp('^.*(' + keyword + ').*$', 'i');
+      return regex.test(product.getName());
+    });
+  }
 }
--- codebase/frontend/src/app/catalogo/catalogo.service.ts
+++ codebase/frontend/src/app/catalogo/catalogo.service.ts
@@ -1,13 +1,21 @@
 import { HttpCommonService } from '../shared/http-common.service';
-import { Category } from '../shared/model/category';
+import { ICategory } from '../shared/model/category';
+import { IProduct } from '../shared/model/product';
 
 // tslint:disable: align
 export class CatalogoService {
   private updateCustomerPath = 'http://localhost:8080/api/categories/';
+  private getProductsPath = 'http://localhost:8080/api/products/';
 
-  constructor(private http: HttpCommonService) { }
-
-  public getCategories(): Promise<Array<Category>> {
-    return this.http.getRequest(this.updateCustomerPath);
+  constructor(private http: HttpCommonService) {
   }
+
+  public getCategories(): Promise<ICategory[]> {
+    return this.http.getRequest(this.updateCustomerPath) as Promise<ICategory[]>;
+  }
+
+  public getProducts(): Promise<IProduct[]> {
+    return this.http.getRequest(this.getProductsPath) as Promise<IProduct[]>;
+  }
+  
 }
