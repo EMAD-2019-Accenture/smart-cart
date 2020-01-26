b0dab7823d84da6e15bab36f691678b5f6d8ee53
emaiannone <emaiannone96@gmail.com>
2020-01-26 21:45:55 +0100

    Fix decimal digits. Add draft hardware back button logic

--- codebase/frontend/src/app/app.component.ts
+++ codebase/frontend/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+// tslint:disable: max-line-length
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Router } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { AuthService } from './auth/auth.service';
-import { Router } from '@angular/router';
+import { ActionSheetController, IonRouterOutlet, MenuController, ModalController, Platform, PopoverController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +11,21 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  private lastTimeBackPress = 0;
+  private timePeriodToExit = 2000;
+
+  @ViewChildren(IonRouterOutlet)
+  private routerOutlets: QueryList<IonRouterOutlet>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private authService: AuthService,
+    private actionSheetCtrl: ActionSheetController,
+    private popoverCtrl: PopoverController,
+    private modalCtrl: ModalController,
+    private menuCtrl: MenuController,
+    private toastCtrl: ToastController,
     private router: Router
   ) {
     this.initializeApp();
@@ -26,6 +35,79 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.initializeBackButton();
+    });
+  }
+
+  // TODO: Does this work?
+  initializeBackButton() {
+    this.platform.backButton.subscribe(async () => {
+      // close action sheet
+      try {
+        const element = await this.actionSheetCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      // close popover
+      try {
+        const element = await this.popoverCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      // close modal
+      try {
+        const element = await this.modalCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      // close side menu
+      try {
+        const element = await this.menuCtrl.getOpen();
+        if (element !== null) {
+          this.menuCtrl.close();
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      this.routerOutlets.forEach(async (outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+        } else if (this.router.url === '/index/**') {
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            // this.platform.exitApp(); // Exit from app
+            navigator['app'].exitApp(); // work in ionic 4
+          } else {
+            // TODO: Use Custom ToastService
+            const toast = await this.toastCtrl.create(
+              {
+                message: 'Premi di nuovo Indietro per uscire',
+                duration: 2000,
+                showCloseButton: false,
+                translucent: true,
+                color: 'green'
+              });
+            toast.present();
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        }
+      });
     });
   }
 }
